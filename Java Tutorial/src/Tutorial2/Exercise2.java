ce525dbd9a1e1cb4cdff1f5fa31f2433590f7e27
HighwayZ <asino1990@live.com>
2016-09-29 22:07:55 +0100

    First commit

---
+++ Java Tutorial/src/Tutorial2/Exercise2.java
@@ -0,0 +1,12 @@
+package Tutorial2;
+
+public class Exercise2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello world! \n");
+		System.out.print("9/29/2016\n");
+		System.out.println("Hello world! \n");
+		System.out.println("9/29/2016");
+
+	}
+}
